0ed39fcaf4963438a03588f88ab9b37cdbfc981c
PK <hotind93@gmail.com>
2023-03-08 16:31:07 +0700

    Сама команда

---
+++ app/Console/Commands/SetWebHook.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class SetWebHook extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'webhook:set
+    {link : The link for webhook}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command set webhook';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $response = Http::get('https://api.telegram.org/bot' . config('telegram.bots.mybot.token')
+            . '/setWebhook?url=' . $this->argument('link'));
+        if (json_decode($response->body())->ok) {
+            $this->info('Webhook command was successful!');
+            return Command::SUCCESS;
+        } else {
+            $this->error('Webhook command was error!');
+            $this->error(json_decode($response->body())->description);
+            return Command::FAILURE;
+        }
+    }
+}
